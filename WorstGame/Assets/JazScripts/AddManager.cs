7f3e0c3b226cfcef66577b9e01bfa70084c903f8
Skibbalt <jazw090@gmail.com>
2024-07-29 21:32:07 -0600

    Adds and Player controller

---
+++ WorstGame/Assets/JazScripts/AddManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] adPrefabs; // Array of ad prefabs
+    [SerializeField] private Transform[] spawnPoints; // Array of spawn points
+    [SerializeField] private Canvas uiCanvas; // Reference to the UI canvas
+
+    private void OnEnable()
+    {
+        // Register the event when the player jumps
+        PlayerMovement.OnJump += SpawnAd;
+    }
+
+    private void OnDisable()
+    {
+        // Unregister the event when the player jumps
+        PlayerMovement.OnJump -= SpawnAd;
+    }
+
+    private void SpawnAd()
+    {
+        // Get a random ad prefab and spawn point
+        int randomAdIndex = UnityEngine.Random.Range(0, adPrefabs.Length);
+        int randomSpawnIndex = UnityEngine.Random.Range(0, spawnPoints.Length);
+
+        // Instantiate the ad at the spawn point under the UI canvas
+        GameObject adInstance = Instantiate(adPrefabs[randomAdIndex], spawnPoints[randomSpawnIndex].position, Quaternion.identity, uiCanvas.transform);
+    }
+}
---
+++ WorstGame/Assets/JazScripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private float horizontal;
+    private bool isFacingRight = true;
+    public static event System.Action OnJump; // Event for when the player jumps
+
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpingPower = 16f;
+
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if (!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            OnJump?.Invoke(); // Trigger the OnJump event
+        }
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+}
